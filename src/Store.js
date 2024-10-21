4edc12ebe6e6d93b878e133743d8e554a4d503ab
Shamini1606 <shaminisiva16@gmail.com>
2024-10-21 13:35:08 +0530

    simple add, delete, list customer all done

--- src/Store.js
+++ src/Store.js
@@ -2,7 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import customerReducer from "./slices/customerSlice";
 
 export const store = configureStore({
+  devTools: false,
   reducer: {
-    customers: customerReducer
+    customers: customerReducer,
   },
 });
